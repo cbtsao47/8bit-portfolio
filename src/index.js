baacf55c09d645bad2bb7abeaa05b9dd78361b0e
Ben <cbtsao47@gmail.com>
2019-07-06 01:39:19 -0500

    remove CDN and add dependency

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import "nes.css/css/nes.css";
 ReactDOM.render(
   <Router>
     <App />
