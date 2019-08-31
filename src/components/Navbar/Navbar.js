781f50eecc0ec55076bd1f15f2350b20e0ff3c12
Lambda_School_Loaner_62 <lambda_school_loaner_62@LambdaSer62sMBP.attlocal.net>
2019-08-31 14:10:59 -0500

    complete tablet view

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,18 +4,32 @@ import Social from "../Social/Social";
 import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
-    <nav className="navbar nes-container is-dark is-rounded">
-      <NavLink exact to="/" className="nes-btn" activeClassName="is-primary">
-        Home
-      </NavLink>
-      <NavLink to="/about" className="nes-btn " activeClassName="is-primary">
-        About
-      </NavLink>
-      <NavLink to="/projects" className="nes-btn" activeClassName="is-primary">
-        Projects
-      </NavLink>
-      <Social />
-    </nav>
+    <div className='navbar-container'>
+      <nav className="navbar nes-container is-dark is-rounded">
+        <NavLink exact to="/" className="nes-btn" activeClassName="is-primary">
+          Home
+        </NavLink>
+        <NavLink to="/about" className="nes-btn " activeClassName="is-primary">
+          About
+        </NavLink>
+        <NavLink to="/projects" className="nes-btn" activeClassName="is-primary">
+          Projects
+        </NavLink>
+        <Social />
+      </nav>
+      <nav className='mobile nes-container is-dark is-rounded'>
+        <NavLink exact to="/" className="nes-btn" activeClassName="is-primary">
+            Home
+          </NavLink>
+          <NavLink to="/about" className="nes-btn " activeClassName="is-primary">
+            About
+          </NavLink>
+          <NavLink to="/projects" className="nes-btn" activeClassName="is-primary">
+            Projects
+        </NavLink>
+        <Social />
+      </nav>
+    </div>
   );
 };
 
